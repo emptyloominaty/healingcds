f88eabf8ca00fd58733e7eda00186da7167fad0c
emptyloominaty <cipuksiblol@gmail.com>
2020-07-21 04:20:48 +0200

    v0.0000002

---
+++ src/components/AddHealingCd.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "AddHealingCd"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/List-HealingCds.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "List-HealingCds"
+    }
+</script>
+
+<style scoped>
+
+</style>
